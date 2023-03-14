684ae8873d0c863ad701f1878cf792fbe16bce2b
PC HONG <peterhpc29@gmail.com>
2023-03-14 15:06:33 +0800

    test framework

---
+++ Spring/SpringBoot/AssemblerFramework/src/test/java/com/example/assemblerframework/AssemblerFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assemblerframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblerFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
