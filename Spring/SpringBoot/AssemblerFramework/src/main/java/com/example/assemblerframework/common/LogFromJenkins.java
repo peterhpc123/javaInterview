5f4c677295ef5ed13ae3447d5640cf31d8948ac9
peterhpc123 <peterhpc29@gmail.com>
2023-03-21 14:08:04 +0800

    Delete Spring/SpringBoot/AssemblerFramework directory

--- Spring/SpringBoot/AssemblerFramework/src/main/java/com/example/assemblerframework/common/LogFromJenkins.java
+++
@@ -1,9 +0,0 @@
-package com.example.assemblerframework.common;
-/*
-* Author: peter
-* Description:
-*
-* */
-public class LogFromJenkins {
-    //TODO: realize a function to get all url form jenkins and get some data from it.
-}
